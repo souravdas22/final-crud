6351bda0dc64ad68b4f2f47ee8525f8b470c8e3f
Coolsourav <9163905324das@gmail.com>
2024-07-07 23:38:31 +0530

    all

--- app/config/db.js
+++ app/config/db.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 const dbConnecton = async () => {
     try {
-      const connection = await mongoose.connect(process.env.MONGO_URL);
+      const connection = await mongoose.connect(
+        "mongodb+srv://souravdas91:C8zCvBRpy3ZU7sor@crud.hbuvcvm.mongodb.net/crud_app"
+      );
       if (connection) {
         console.log("Connected to mongodb");
       }
